5c24c7bf6dee54b4df7f9b17539ba6e140fa867e
Amy Milo <amymmilo@gmail.com>
2016-09-11 16:14:04 -0400

    layout changes

--- controllers/RootController.go
+++ controllers/RootController.go
@@ -11,9 +11,9 @@ import (
 
 // RootController handles main homepage functions
 func RootController(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("views/index.html")
+	t, err := template.ParseFiles("views/home.html")
 	utils.CheckErr(err)
-	p := models.Page{Title: "home!"}
+	p := models.Page{Title: "Amy and Patrick's Wedding!"}
 	t.Execute(w, p)
 	fmt.Println(r.Host)
 	// Simple page serve: fmt.Fprintf(w, "homepage!")
--- router.go
+++ router.go
@@ -14,6 +14,7 @@ func registerControllers() {
 
 	// All the rest...
 	http.HandleFunc("/static/", controllers.StaticController)
+	
 	http.HandleFunc("/", controllers.RootController)
 
 }
